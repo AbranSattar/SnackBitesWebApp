a25c499efa09f3a67d318cc60562808ed58f2b7c
ac148776 <ac148776@A27-GM0Z64BN>
2026-03-13 10:04:28 +1300

    Add project files.

---
+++ SnackBitesWebApp/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SnackBitesWebApp.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SnackBitesWebAppContextConnection") ?? throw new InvalidOperationException("Connection string 'SnackBitesWebAppContextConnection' not found.");;
+
+builder.Services.AddDbContext<SnackBitesWebAppContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<SnackBitesWebAppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<SnackBitesWebAppContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
