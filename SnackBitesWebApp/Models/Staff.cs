a1016bd49c7028580c3742344bcfcb1d2f1a949d
Abran Sattar <ac148776@avcol.school.nz>
2026-03-17 14:05:48 +1300

    created the staff class and populated it.

---
+++ SnackBitesWebApp/Models/Staff.cs
@@ -0,0 +1,10 @@
+﻿namespace SnackBitesWebApp.Models
+{
+    public class Staff
+    {
+        public int StaffID { get; set; }
+        public string? StaffName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+    }
+}
