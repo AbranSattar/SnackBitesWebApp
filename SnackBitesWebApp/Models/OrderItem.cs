5e691cdd189a979d41b2823440b6db789d41217e
Abran Sattar <ac148776@avcol.school.nz>
2026-03-17 14:15:14 +1300

    added the foreign keys to the orderItem class

--- SnackBitesWebApp/Models/OrderItem.cs
+++ SnackBitesWebApp/Models/OrderItem.cs
@@ -1,4 +1,6 @@
-﻿namespace SnackBitesWebApp.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SnackBitesWebApp.Models
 {
     public class OrderItem
     {
@@ -6,5 +8,11 @@
         public int OrderID { get; set; }
         public int FoodID { get; set; } 
         public int Quantity { get; set; }
+
+        [ForeignKey("OrderID")]
+        public Order Order { get; set; }
+
+        [ForeignKey("FoodID")]
+        public Food Food { get; set; }
     }
 }
