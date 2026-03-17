3d8484dcd9c2460b2c116c2647fb900c82c44814
Abran Sattar <ac148776@avcol.school.nz>
2026-03-17 13:54:58 +1300

    created the orderItem class and the order class. and populated all of the classes with correct columns.

---
+++ SnackBitesWebApp/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SnackBitesWebApp.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int SnackBitesWebAppUserID { get; set; }
+        public int StaffID { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int OrderStatus { get; set; }
+    }
+}
---
+++ SnackBitesWebApp/Models/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace SnackBitesWebApp.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemID { get; set; }
+        public int OrderID { get; set; }
+        public int FoodID { get; set; } 
+        public int Quantity { get; set; }
+    }
+}
